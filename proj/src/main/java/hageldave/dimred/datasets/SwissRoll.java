59665925ba700d80d6cfca0e3cf23247cd0cc6b7
Luca Reichmann <luca@lucareichmann.de>
2021-12-17 00:24:11 +0100

    added getAllOfClass() for "rolled" data

--- proj/src/main/java/hageldave/dimred/datasets/SwissRoll.java
+++ proj/src/main/java/hageldave/dimred/datasets/SwissRoll.java
@@ -9,6 +9,11 @@ import java.util.stream.IntStream;
 
 public class SwissRoll {
 
+    public enum DATA_TYPE {
+        PRE,
+        ROLL
+    }
+
     private static SwissRoll instance;
     private static final String PRE_SRC_DATA = "https://people.cs.uchicago.edu/~dinoj/manifold/preswissroll.dat";
     private static final String PRE_SRC_LBL = "https://people.cs.uchicago.edu/~dinoj/manifold/preswissroll_labels.dat";
@@ -20,9 +25,11 @@ public class SwissRoll {
 
     public final double[][] preData;
     public final double[][] rollData;
-    public final int[] klass;
+    public final int[] preKlass;
+    public final int[] rollKlass;
     public final String[] klassNames = new String[]{"1","2","3","4"};
-    final int[][] klass2Indices = new int[4][];
+    final int[][] preKlass2Indices = new int[4][];
+    final int[][] rollKlass2Indices = new int[4][];
 
     private SwissRoll() {
         ArrayList<double[]> pre_dataset = new ArrayList<>();
@@ -32,6 +39,7 @@ public class SwissRoll {
                 Scanner preLabelSC = new Scanner(FileHandler.getFile(PRE_SRC_LBL, FILE_NAME_LBL));
                 Scanner rollDataSC = new Scanner(FileHandler.getFile(ROLL_SRC_DATA, ROLL_FILE_NAME_DATA))
         ) {
+            ArrayList<String> label_fields = new ArrayList<>();
             while (preDataSC.hasNextLine() && preLabelSC.hasNextLine()) {
                 String dataNextLine = preDataSC.nextLine();
                 String labelNextLine = preLabelSC.nextLine();
@@ -45,6 +53,8 @@ public class SwissRoll {
                 dataFields = Arrays.stream(dataFields).filter(e->!e.equals("")).toArray(String[]::new);
                 labelFields = Arrays.stream(labelFields).filter(e->!e.equals("")).toArray(String[]::new);
 
+                label_fields.add(labelFields[0]);
+
                 double[] values = new double[3];
                 values[0] = Double.parseDouble(dataFields[0]);
                 values[1] = Double.parseDouble(dataFields[1]);
@@ -53,33 +63,43 @@ public class SwissRoll {
                 pre_dataset.add(values);
             }
 
+
             while (rollDataSC.hasNextLine()) {
                 String rollNextLine = rollDataSC.nextLine();
+
                 if (rollNextLine.isEmpty()) {
                     continue;
                 }
                 String[] dataFields = rollNextLine.split(" ");
+
                 dataFields = Arrays.stream(dataFields).filter(e->!e.equals("")).toArray(String[]::new);
-                double[] values = new double[3];
+
+                double[] values = new double[4];
                 values[0] = Double.parseDouble(dataFields[0]);
                 values[1] = Double.parseDouble(dataFields[1]);
                 values[2] = Double.parseDouble(dataFields[2]);
+                values[3] = Double.parseDouble(label_fields.get(0));
                 roll_dataset.add(values);
             }
         }
 
         rollData = roll_dataset.stream().map(v -> Arrays.copyOf(v, 3)).toArray(double[][]::new);
-        preData = pre_dataset.stream().map(v -> Arrays.copyOf(v, 3)).toArray(double[][]::new);
-        klass = pre_dataset.stream().mapToInt(v -> (int)v[2]).toArray();
+        preData = pre_dataset.stream().map(v -> Arrays.copyOf(v, 2)).toArray(double[][]::new);
+        preKlass = pre_dataset.stream().mapToInt(v -> (int)v[2]).toArray();
+        rollKlass = roll_dataset.stream().mapToInt(v -> (int)v[3]).toArray();
         for(int t=0; t<4; t++) {
             int t_=t;
-            klass2Indices[t] = IntStream.range(0, klass.length).filter(i->klass[i]==t_).toArray();
+            preKlass2Indices[t] = IntStream.range(0, preKlass.length).filter(i-> preKlass[i]==t_).toArray();
+            rollKlass2Indices[t] = IntStream.range(0, rollKlass.length).filter(i-> rollKlass[i]==t_).toArray();
         }
     }
 
-    // TODO: auch für roll data
-    public double[][] getAllOfClass(int type){
-        return Arrays.stream(klass2Indices[type]).mapToObj(i-> preData[i]).toArray(double[][]::new);
+    public double[][] getAllOfClass(DATA_TYPE dataType, int type) {
+        if (dataType == DATA_TYPE.ROLL) {
+            return Arrays.stream(rollKlass2Indices[type]).mapToObj(i-> rollData[i]).toArray(double[][]::new);
+        } else {
+            return Arrays.stream(preKlass2Indices[type]).mapToObj(i-> preData[i]).toArray(double[][]::new);
+        }
     }
 
     public double[][] getRollData(){
@@ -93,14 +113,12 @@ public class SwissRoll {
     }
 
     public int getNumClasses() {
-        return klass2Indices.length;
+        return preKlass2Indices.length;
     }
 
     public static void main(String[] args) {
         SwissRoll set = SwissRoll.getInstance();
-        System.out.println(Arrays.deepToString(set.getAllOfClass(2)));
-        System.out.println(Arrays.deepToString(set.getRollData()));
+        //System.out.println(Arrays.deepToString(set.getAllOfClass(DATA_TYPE.ROLL, 1)));
+        System.out.println(Arrays.deepToString(set.getAllOfClass(DATA_TYPE.PRE, 1)));
     }
-
-
 }
