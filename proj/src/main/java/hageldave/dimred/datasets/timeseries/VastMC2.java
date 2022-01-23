9379819b07da40bc7d8ce5e2606ec688413c5746
Luca Reichmann <luca@lucareichmann.de>
2022-01-23 01:18:02 +0100

    added internal classes so that the string arrays can be replaced in the mappings

--- proj/src/main/java/hageldave/dimred/datasets/timeseries/VastMC2.java
+++ proj/src/main/java/hageldave/dimred/datasets/timeseries/VastMC2.java
@@ -5,10 +5,7 @@ import FileHandler.FileHandler;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class VastMC2 {
     private static VastMC2 instance;
@@ -17,18 +14,31 @@ public class VastMC2 {
     private static final String FILE_NAME_DATASET = "Boonsong Lekagul waterways readings.csv";
     private static final String FILE_NAME_UNIT_MAPPING = "chemical units of measure.csv";
 
-    private final static ArrayList<String[]> data = new ArrayList<>();
-    private final static HashMap<LocalDate, ArrayList<String[]>> time2data = new HashMap<>();
-    private final static HashMap<String, ArrayList<String[]>> location2data = new HashMap<>();
-    private final static HashMap<String, String> measure2unit = new HashMap<>();
+    private final ArrayList<String[]> data = new ArrayList<>();
+    private final HashMap<LocalDate, ArrayList<TypeDate2Data>> date2data = new HashMap<>();
+    private final HashMap<Integer, ArrayList<TypeLocation2Data>> location2data = new HashMap<>();
+    private final HashMap<String, String> measure2unit = new HashMap<>();
 
+    // the order in these arrays also define the used indices
     public final String[] klassLocationNames = new String[]{"Boonsri", "Kannika", "Chai", "Kohsoom", "Somchair", "Sakda", "Busarakhan", "Tansanee", "Achara", "Decha"};
     public final String[] klassMeasureNames;
+    // public final LocalDate[] klassDateNames;
 
     private VastMC2() {
+        try (Scanner sc = new Scanner(FileHandler.getFileFromZIP(SRC_URL, "", FILE_NAME_UNIT_MAPPING, StandardCharsets.ISO_8859_1))) {
+            while(sc.hasNextLine()) {
+                String nextLine = sc.nextLine();
+                if (nextLine.isEmpty()) {
+                    continue;
+                }
+                String[] fields = nextLine.split(",");
+                measure2unit.put(fields[0], fields[1]);
+            }
+            klassMeasureNames = measure2unit.keySet().toArray(new String[0]);
+        }
+
         // directory is "" because files are in root directory
         try (Scanner sc = new Scanner(FileHandler.getFileFromZIP(SRC_URL, "", FILE_NAME_DATASET))) {
-            ArrayList<String> collectMeasures = new ArrayList<>(103);
             boolean firstLineRead = false;
             while(sc.hasNextLine()) {
                 String nextLine = sc.nextLine();
@@ -41,61 +51,59 @@ public class VastMC2 {
                 // add to data
                 data.add(fields);
 
-                // fill klassMeasureNames array, so that the developer can see what "measure" classes are available
-                if (!collectMeasures.contains(fields[4])) {
-                    collectMeasures.add(fields[4]);
-                }
-
                 // parse date
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yy").withLocale(Locale.US);
-                LocalDate dateTime = LocalDate.parse(fields[3], formatter);
+                LocalDate localDate = LocalDate.parse(fields[3], formatter);
+
+                // get indices of data
+                int locationIndex = Arrays.asList(klassLocationNames).indexOf(fields[2]);
+                int measureIndex = Arrays.asList(klassMeasureNames).indexOf(fields[4]);
 
                 // put stuff into maps
-                if (time2data.containsKey(dateTime))
-                    time2data.get(dateTime).add(new String[]{fields[0], fields[1], fields[2], fields[3]});
+                TypeDate2Data date2Data = new TypeDate2Data(
+                        Integer.parseInt(fields[0]),
+                        Double.parseDouble(fields[1]),
+                        locationIndex,
+                        measureIndex
+                );
+                if (date2data.containsKey(localDate))
+                    date2data.get(localDate).add(date2Data);
                 else {
-                    ArrayList<String[]> entriesForDate = new ArrayList<>();
-                    entriesForDate.add(new String[]{fields[0], fields[1], fields[2], fields[3]});
-                    time2data.put(dateTime, entriesForDate);
+                    ArrayList<TypeDate2Data> dateList = new ArrayList<>();
+                    dateList.add(date2Data);
+                    date2data.put(localDate, dateList);
                 }
 
-                if (location2data.containsKey(fields[2]))
-                    location2data.get(fields[2]).add(new String[]{fields[0], fields[1], fields[2], fields[3]});
+                TypeLocation2Data location2Data = new TypeLocation2Data(
+                        Integer.parseInt(fields[0]),
+                        Double.parseDouble(fields[1]),
+                        localDate,
+                        measureIndex);
+                if (location2data.containsKey(locationIndex))
+                    location2data.get(locationIndex).add(location2Data);
                 else {
-                    ArrayList<String[]> entriesForLoc = new ArrayList<>();
-                    entriesForLoc.add(new String[]{fields[0], fields[1], fields[3], fields[4]});
-                    location2data.put(fields[2], entriesForLoc);
+                    ArrayList<TypeLocation2Data> locationList = new ArrayList<>();
+                    locationList.add(location2Data);
+                    location2data.put(locationIndex, locationList);
                 }
-            }
-            klassMeasureNames = collectMeasures.toArray(String[]::new);
-        }
-
-
-        try (Scanner sc = new Scanner(FileHandler.getFileFromZIP(SRC_URL, "", FILE_NAME_UNIT_MAPPING, StandardCharsets.ISO_8859_1))) {
-            while(sc.hasNextLine()) {
-                String nextLine = sc.nextLine();
-                if (nextLine.isEmpty()) {
-                    continue;
-                }
-                String[] fields = nextLine.split(",");
-                measure2unit.put(fields[0], fields[1]);
             }
         }
     }
 
-    public static ArrayList<String[]> getAllOfDate(LocalDate date) {
-        return time2data.get(date);
+    public ArrayList<TypeDate2Data> getAllOfDate(LocalDate date) {
+        return date2data.get(date);
     }
 
-    public static ArrayList<String[]> getAllOfLocation(String location) {
+    public ArrayList<TypeLocation2Data> getAllOfLocation(int location) {
         return location2data.get(location);
     }
 
-    public static ArrayList<String[]> getData() {
+
+    public ArrayList<String[]> getData() {
         return data;
     }
 
-    public static String getUnitOfMeasure(String measure) {
+    public String getUnitOfMeasure(String measure) {
         return measure2unit.get(measure);
     }
 
@@ -105,4 +113,95 @@ public class VastMC2 {
         return instance;
     }
 
+    public static class TypeLocation2Data {
+        private int id;
+        private double value;
+        private LocalDate sampleDate;
+        private int measure;
+
+        public TypeLocation2Data(int id, double value, LocalDate sampleDate, int measure) {
+            this.id = id;
+            this.value = value;
+            this.sampleDate = sampleDate;
+            this.measure = measure;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
+        }
+
+        public LocalDate getSampleDate() {
+            return sampleDate;
+        }
+
+        public void setSampleDate(LocalDate sampleDate) {
+            this.sampleDate = sampleDate;
+        }
+
+        public int getMeasure() {
+            return measure;
+        }
+
+        public void setMeasure(int measure) {
+            this.measure = measure;
+        }
+    }
+
+    public static class TypeDate2Data {
+        private int id;
+        private double value;
+        private int location;
+        private int measure;
+
+        public TypeDate2Data(int id, double value, int location, int measure) {
+            this.id = id;
+            this.value = value;
+            this.location = location;
+            this.measure = measure;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
+        }
+
+        public int getLocation() {
+            return location;
+        }
+
+        public void setLocation(int location) {
+            this.location = location;
+        }
+
+        public int getMeasure() {
+            return measure;
+        }
+
+        public void setMeasure(int measure) {
+            this.measure = measure;
+        }
+    }
 }
